d5dc8bde67b8fd1f278472cac1708226d2a10444
nagendrayakkaladevara <nagendrayakkaladevara@gmail.com>
2025-03-10 15:40:53 +0530

    getblogendpoint correction & rate limiter message correction

--- src/middlewares/v1/rateLimiter.ts
+++ src/middlewares/v1/rateLimiter.ts
@@ -3,7 +3,8 @@ import rateLimit from "express-rate-limit";
 export const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 10, // Limit each IP to 100 requests per `windowMs`
-  message: { message: "Too many requests, please try again later." },
+  // message: { message: "You've made too many requests from this IP.Please try again later🙂." },
+  message: { message: "Too many requests, please try again later🙂." },
   headers: true, // Send RateLimit headers in the response
   standardHeaders: "draft-7", // Use standard rate limit headers
   legacyHeaders: false, // Disable deprecated headers
